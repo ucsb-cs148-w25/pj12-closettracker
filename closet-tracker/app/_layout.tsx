a64e5cc94d3aca86c1881956c1d898b97c87395c
Gen Tamada <gtamada@ucsb.edu>
2025-03-05 13:17:54 -0800

    fixing android status bar warning, and like/unlike heart icon

--- closet-tracker/app/_layout.tsx
+++ closet-tracker/app/_layout.tsx
@@ -1,4 +1,5 @@
 import { Slot } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 import { UserProvider } from '@/context/UserContext';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
@@ -7,6 +8,7 @@ export default function Root() {
   return (
     <GestureHandlerRootView>
       <UserProvider>
+        <StatusBar style="dark" />
         <Slot/>
       </UserProvider>
     </GestureHandlerRootView>
