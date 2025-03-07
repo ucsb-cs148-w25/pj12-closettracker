e334746d6b0c64129ed51cb708f09e551e8cc7e0
Gen Tamada <gtamada@ucsb.edu>
2025-03-07 15:18:47 -0800

    refresh

--- closet-tracker/app/_layout.tsx
+++ closet-tracker/app/_layout.tsx
@@ -4,7 +4,6 @@ import { UserProvider } from '@/context/UserContext';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function Root() {
-  // Set up the auth context and render our layout inside of it.
   return (
     <GestureHandlerRootView>
       <UserProvider>
