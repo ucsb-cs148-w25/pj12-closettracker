841cf5ef5e9bf213c1197a6dcdf5f7f63764ad0f
Gen Tamada <gtamada@ucsb.edu>
2025-03-06 22:03:04 -0800

    moving new main into styling branch

--- closet-tracker/app/(login)/login.tsx
+++ closet-tracker/app/(login)/login.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, ActivityIndicator, TouchableHighlight, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, TextInput, ActivityIndicator, StyleSheet, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -34,7 +34,7 @@ export default function Login() {
                 <TextInput
                     style={styles.input}
                     placeholder="Email"
-                    placeholderTextColor="#FFFFFF"
+                    placeholderTextColor={beigeColors.taupe}
                     value={email}
                     onChangeText={setEmail}
                     autoCapitalize="none"
@@ -44,7 +44,7 @@ export default function Login() {
                 <TextInput
                     style={styles.input}
                     placeholder="Password"
-                    placeholderTextColor="#FFFFFF"
+                    placeholderTextColor={beigeColors.taupe}
                     value={password}
                     onChangeText={setPassword}
                     secureTextEntry
@@ -53,9 +53,9 @@ export default function Login() {
                 {loading ? (
                     <ActivityIndicator size="large" color={beigeColors.mutedGold} />
                 ) : (
-                    <TouchableHighlight style={styles.button} onPress={signIn}>
+                    <TouchableOpacity style={styles.button} onPress={signIn}>
                         <Text style={styles.buttonText}>Login</Text>
-                    </TouchableHighlight>
+                    </TouchableOpacity>
                 )}
             </ScrollView>
         </SafeAreaView>
@@ -78,7 +78,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginBottom: 20,
         textAlign: 'center',
-        color: '#FFFFFF',
+        color: beigeColors.darkBeige,
     },
     input: {
         width: '100%',
@@ -90,7 +90,7 @@ const styles = StyleSheet.create({
         fontSize: 16,
         textAlign: 'center',
         backgroundColor: beigeColors.softBrown,
-        color: '#FFFFFF',
+        color: beigeColors.darkBeige,
     },
     button: {
         backgroundColor: beigeColors.taupe,
@@ -101,8 +101,7 @@ const styles = StyleSheet.create({
         marginTop: 10,
     },
     buttonText: {
-        color: '#FFFFFF',
+        color: beigeColors.darkBeige,
         fontSize: 16,
-        fontWeight: 'bold',
     },
 });
